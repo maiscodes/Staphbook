38ceb3cb0257f3d8b620d257732cb2356503d63b
Andrew <chodydragonborn@gmail.com>
2018-09-15 14:31:46 +1000

    Added Pestgres Functionality

--- Nodewebsite/app.js
+++ Nodewebsite/app.js
@@ -1,7 +1,33 @@
 // load the things we need
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
+//postgreSQL
+const { Pool, Client } = require('pg');
+const connectionString = 'postgresql://postgres:password@127.0.0.1:5432/Staphopia';
+
+const pool = new Pool({
+    connectionString: connectionString,
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res);
+    pool.end();
+});
+
+const client = new Client({
+    connectionString: connectionString,
+});
+client.connect();
+
+//BodyParser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+//Cytoscape.js
 app.use('/cytoscape_scripts', express.static(__dirname + '/node_modules/cytoscape/dist/'));
 app.use('/cola_scripts', express.static(__dirname + '/node_modules/cytoscape-cola/'));
 
@@ -31,8 +57,17 @@ app.get('/result', function(req, res) {
 });
 
 // search results page
-app.get('/searchResults', function(req, res) {
-    res.render('pages/searchResults');
+app.post('/searchResults', function(req, res) {
+    console.log(req.body.search.input);
+    let title_id = 0;
+    client.query("SELECT id FROM staphopia_blastquery WHERE title='"+ req.body.search.input + "'",(err, result)=> {
+        console.log(err, result);
+        title_id = result.rows[0].id;
+        client.query('SELECT * FROM sccmec_primers WHERE query_id='+ title_id + '', (err, result) => {
+            console.log(err, result);
+            res.render('pages/searchResults', {sccmec_primers: result.rows});
+        });
+    });
 });
 
 app.listen(8000);
