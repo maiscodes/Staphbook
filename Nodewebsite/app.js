4dafdc79fd6655b7a31b02ea9eb61cf2e00b8b5a
Andrew <chodydragonborn@gmail.com>
2018-10-02 19:09:53 +1000

    Added functionality for adcaned search

--- Nodewebsite/app.js
+++ Nodewebsite/app.js
@@ -10,8 +10,8 @@ app.use(cookieParser('xoxoGossipGirl'));
 //postgreSQL
 const { Pool, Client } = require('pg');
 //layout is const connectionString = 'postgresql://username:password@address/Database_name';
-const connectionString = 'postgresql://postgres:12345@127.0.0.1:5432/staph';
-//const connectionString = 'postgresql://postgres:password@127.0.0.1:5432/Staphopia';
+//const connectionString = 'postgresql://postgres:12345@127.0.0.1:5432/staph';
+const connectionString = 'postgresql://postgres:password@127.0.0.1:5432/Staphopia';
 
 const pool = new Pool({
     connectionString: connectionString,
@@ -201,11 +201,87 @@ app.get('/searchResults', function (req, res) {
 				number = result_samples.rows.length;
 				res.render('pages/searchResults', { samples: result_samples.rows, input: input, option: option, number: number, userLoggedIn: userLoggedIn });
 			});
-		}
+        } else if(option == "Sample"){
+            client.query("SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
+                "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
+                "sample_metadata.metadata->>'isolation_source' AS isolation_source " +
+                "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
+                "WHERE mlst_mlst.sample_id IN (SELECT sample_id FROM sample_metadata " +
+                "WHERE sample_id = '" + input + "')", (err, result_samples) => {
+                console.log(err, result_samples);
+                number = result_samples.rows.length;
+                res.render('pages/searchResults', { samples: result_samples.rows, input: input, option: option, number: number, userLoggedIn: userLoggedIn });
+            });
+        }
 	} else {
 		res.redirect('/');
 	}
 });
+
+// Advanced search results page
+app.get('/advSearchResults', function (req, res) {
+    if (req.headers.cookie) {
+        userLoggedIn = true;
+        console.log('req headers cookie if statement cookie test @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+
+    } else {
+        userLoggedIn = false;
+    }
+
+    let sequenceInput = req.query.sequenceInput;
+    let locationInput = req.query.locationInput;
+    let strainInput = req.query.strainInput;
+    let hostInput = req.query.hostInput;
+    let sourceInput = req.query.sourceInput;
+    if(strainInput == "") {
+        //strainInput=null;
+    }
+    let selectSQL = "";
+    selectSQL = "SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
+        "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
+        "sample_metadata.metadata->>'isolation_source' AS isolation_source " +
+        "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
+        "WHERE";
+    if (sequenceInput != ""){
+        selectSQL += " mlst_mlst.st = '" + sequenceInput + "'";
+        if (locationInput != "" || strainInput != "" || hostInput != "" || sourceInput != ""){
+            selectSQL += " AND ";
+        }
+    }
+    if (locationInput != ""){
+        selectSQL += "metadata->>'country' LIKE '%" + locationInput + "%'";
+        if (strainInput != "" || hostInput != "" || sourceInput != ""){
+            selectSQL += " AND ";
+        }
+    }
+    if (strainInput != ""){
+        selectSQL += "metadata->>'country' LIKE '%" + strainInput + "%'";
+        if (hostInput != "" || sourceInput != ""){
+            selectSQL += " AND ";
+        }
+    }
+    if (hostInput != ""){
+        selectSQL += "metadata->>'host' LIKE '%" + hostInput + "%' AND";
+        if (sourceInput != ""){
+            selectSQL += " AND ";
+        }
+    }
+    if (sourceInput != ""){
+        selectSQL += "metadata->>'isolation_source' LIKE '%" + sourceInput + "%' ";
+    }
+    console.log(selectSQL);
+
+    if (req.query.sequenceInput) {
+        client.query(selectSQL + ";", (err, result_samples) => {
+            console.log(err, result_samples);
+            number = result_samples.rows.length;
+            res.render('pages/advSearchResults', { samples: result_samples.rows, number: number, userLoggedIn: userLoggedIn });
+        });
+    //} else {
+        //res.redirect('/advancedSearch');
+    }
+});
+
 
 app.get('/login', function (req, res) {
 	if (req.headers.cookie) {
