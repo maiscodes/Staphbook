21d1720148437a9edae2756c113f5d800a3223e7
Andrew <chodydragonborn@gmail.com>
2018-10-28 23:41:00 +1000

    Fixed issue where clicking X next to favorites didn't remove from list

--- Nodewebsite/app.js
+++ Nodewebsite/app.js
@@ -1045,48 +1045,49 @@ app.post('/favourites', function (req, res) {
                             throw error;
                         }
                     });
-            }
-        });
-    }
+                // Initial values
+                let favorites = [];
+                let hasFavs = false;
 
-    // Initial values
-    let favorites = [];
-    let hasFavs = false;
+                if (req.session.userStatus == "loggedIn") {
+                    userLoggedIn = true;
+                    let value = req.session.userEmail;
+                    let email = decodeURIComponent(value);
+                    client.query("SELECT * FROM user_favorites WHERE email='" + email + "'", (err, fav_results) => {
+                        console.log(err, fav_results);
+                        if (fav_results.rows.length > 0) {
+                            haveFavs = true;
+                            haveSugs = true;
+                            let selectSQL = "";
+                            selectSQL = "SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
+                                "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
+                                "sample_metadata.metadata->>'isolation_source' AS isolation_source, sample_metadata.metadata->>'date_collected' AS date " +
+                                "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
+                                "WHERE mlst_mlst.sample_id IN (SELECT sample_id FROM sample_metadata " +
+                                "WHERE";
 
-    if (req.session.userStatus == "loggedIn") {
-        userLoggedIn = true;
-        let value = req.session.userEmail;
-        let email = decodeURIComponent(value);
-        client.query("SELECT * FROM user_favorites WHERE email='" + email + "'", (err, fav_results) => {
-            console.log(err, fav_results);
-            if (fav_results.rows.length > 0) {
-                haveFavs = true;
-                haveSugs = true;
-                let selectSQL = "";
-                selectSQL = "SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-                    "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-                    "sample_metadata.metadata->>'isolation_source' AS isolation_source, sample_metadata.metadata->>'date_collected' AS date " +
-                    "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
-                    "WHERE mlst_mlst.sample_id IN (SELECT sample_id FROM sample_metadata " +
-                    "WHERE";
+                            for (let i = 0; i < fav_results.rows.length; i++) {
+                                if (i == 0) {
+                                    selectSQL += " sample_id = " + fav_results.rows[i].sample_id + "";
+                                }
+                                selectSQL += " OR sample_id = " + fav_results.rows[i].sample_id + "";
+                            }
+                            console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ SELECT SQL " + selectSQL + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 
-                for (let i = 0; i < fav_results.rows.length; i++) {
-                    if (i == 0) {
-                        selectSQL += " sample_id = " + fav_results.rows[i].sample_id + "";
-                    }
-                    selectSQL += " OR sample_id = " + fav_results.rows[i].sample_id + "";
+                            client.query(selectSQL + ");", (err, favorites) => {
+                                console.log(err, favorites);
+                                console.log(haveFavs);
+                                res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: favorites.rows, haveFavs: haveFavs });
+                            });
+                        }
+                    });
+
                 }
-                console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ SELECT SQL " + selectSQL + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-
-                client.query(selectSQL + ");", (err, favorites) => {
-                    console.log(err, favorites);
-                    console.log(haveFavs);
-                    res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: favorites.rows, haveFavs: haveFavs });
-                });
             }
         });
 
     }
+
 });
 
 
