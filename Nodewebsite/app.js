dca381a38f77e0e05efe57f9d7572523b3f998a8
Andrew <chodydragonborn@gmail.com>
2018-10-29 20:22:27 +1000

    Fixed advanced search crash

--- Nodewebsite/app.js
+++ Nodewebsite/app.js
@@ -14,10 +14,10 @@ const { Pool, Client } = require('pg');
 //layout is const connectionString = 'postgresql://username:password@address/Database_name';
 
 // Sandra's connection
-const connectionString = 'postgresql://postgres:12345@127.0.0.1:5432/staph';
+//const connectionString = 'postgresql://postgres:12345@127.0.0.1:5432/staph';
 
 // Andrew's connection 
-//const connectionString = 'postgresql://postgres:password@127.0.0.1:5432/Staphopia';
+const connectionString = 'postgresql://postgres:password@127.0.0.1:5432/Staphopia';
 
 const pool = new Pool({
     connectionString: connectionString,
@@ -429,64 +429,62 @@ app.get('/advSearchResults', function (req, res) {
     let strainInput = req.query.strainInput;
     let hostInput = req.query.hostInput;
     let sourceInput = req.query.sourceInput;
-    if(strainInput == "") {
-        //strainInput=null;
-    }
-    let selectSQL = "";
-    selectSQL = "SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-        "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-        "sample_metadata.metadata->>'isolation_source' AS isolation_source, sample_metadata.metadata->>'date_collected' AS date " +
-        "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
-        "WHERE";
-    if (sequenceInput != ""){
-        selectSQL += " mlst_mlst.st = '" + sequenceInput + "'";
-        if (locationInput != "" || strainInput != "" || hostInput != "" || sourceInput != "" || startDate != "" || endDate != ""){
-            selectSQL += " AND ";
+    if(strainInput == "" && sequenceInput=="" && startDate=="" && endDate=="" && locationInput=="" && hostInput=="" && sourceInput=="") {
+        res.redirect('/advancedSearch');
+    } else {
+        let selectSQL = "";
+        selectSQL = "SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
+            "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
+            "sample_metadata.metadata->>'isolation_source' AS isolation_source, sample_metadata.metadata->>'date_collected' AS date " +
+            "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
+            "WHERE";
+        if (sequenceInput != ""){
+            selectSQL += " mlst_mlst.st = '" + sequenceInput + "'";
+            if (locationInput != "" || strainInput != "" || hostInput != "" || sourceInput != "" || startDate != "" || endDate != ""){
+                selectSQL += " AND ";
+            }
         }
-    }
-    if (startDate != "" || endDate != ""){
-        if(endDate == ""){
-            selectSQL += " CAST(metadata->>'collection_date' AS DATE) >= '" + startDate + "'";
-        } else if (startDate == ""){
-            selectSQL += " CAST(metadata->>'collection_date' AS DATE) <= '" + endDate + "'";
-        } else{
-            selectSQL += " CAST(metadata->>'collection_date' AS DATE) >= '" + startDate + "' AND  CAST(metadata->>'collection_date' AS DATE) <= '" + endDate + "'";
+        if (startDate != "" || endDate != ""){
+            if(endDate == ""){
+                selectSQL += " CAST(metadata->>'collection_date' AS DATE) >= '" + startDate + "'";
+            } else if (startDate == ""){
+                selectSQL += " CAST(metadata->>'collection_date' AS DATE) <= '" + endDate + "'";
+            } else{
+                selectSQL += " CAST(metadata->>'collection_date' AS DATE) >= '" + startDate + "' AND  CAST(metadata->>'collection_date' AS DATE) <= '" + endDate + "'";
+            }
+            if (strainInput != "" || hostInput != "" || sourceInput != ""){
+                selectSQL += " AND ";
+            }
         }
-        if (strainInput != "" || hostInput != "" || sourceInput != ""){
-            selectSQL += " AND ";
+        if (locationInput != ""){
+            selectSQL += " metadata->>'country' ILIKE '%" + locationInput + "%'";
+            if (strainInput != "" || hostInput != "" || sourceInput != ""){
+                selectSQL += " AND ";
+            }
         }
-    }
-    if (locationInput != ""){
-        selectSQL += " metadata->>'country' ILIKE '%" + locationInput + "%'";
-        if (strainInput != "" || hostInput != "" || sourceInput != ""){
-            selectSQL += " AND ";
+        if (strainInput != ""){
+            selectSQL += " metadata->>'strain' ILIKE '%" + strainInput + "%'";
+            if (hostInput != "" || sourceInput != ""){
+                selectSQL += " AND ";
+            }
         }
-    }
-    if (strainInput != ""){
-        selectSQL += " metadata->>'strain' ILIKE '%" + strainInput + "%'";
-        if (hostInput != "" || sourceInput != ""){
-            selectSQL += " AND ";
+        if (hostInput != ""){
+            selectSQL += " metadata->>'host' ILIKE '%" + hostInput + "%'";
+            if (sourceInput != ""){
+                selectSQL += " AND ";
+            }
         }
-    }
-    if (hostInput != ""){
-        selectSQL += " metadata->>'host' ILIKE '%" + hostInput + "%'";
         if (sourceInput != ""){
-            selectSQL += " AND ";
+            selectSQL += " metadata->>'isolation_source' ILIKE '%" + sourceInput + "%' ";
         }
-    }
-    if (sourceInput != ""){
-        selectSQL += " metadata->>'isolation_source' ILIKE '%" + sourceInput + "%' ";
-    }
-    console.log(selectSQL);
+        console.log(selectSQL);
 
-    client.query(selectSQL + ";", (err, result_samples) => {
-        console.log(err, result_samples);
-        number = result_samples.rows.length;
-        res.render('pages/advSearchResults', { samples: result_samples.rows, number: number, userLoggedIn: userLoggedIn });
-    });
-    //} else {
-        //res.redirect('/advancedSearch');
-
+        client.query(selectSQL + ";", (err, result_samples) => {
+            console.log(err, result_samples);
+            number = result_samples.rows.length;
+            res.render('pages/advSearchResults', { samples: result_samples.rows, number: number, userLoggedIn: userLoggedIn });
+        });
+    }
 });
 
 
