cfb06f3767c5e1356cc8a66ca0f828a1f4ccd4ae
Sam Holden <samuel99holden@gmail.com>
2020-08-18 16:29:01 +1000

    View group details router

--- Nodewebsite/app.js
+++ Nodewebsite/app.js
@@ -80,6 +80,7 @@ let advSearchResultRouter = require('./routes/advSearchResults');
 let loginRouter = require('./routes/login');
 let favouriteRouter = require('./routes/favourites');
 let groupsRouter = require('./routes/groups');
+let viewGroupRouter = require('./routes/viewGroup')
 
 /* --------------------------------------------------------------------------------
  *
@@ -101,88 +102,7 @@ app.use('/advSearchResults', advSearchResultRouter);
 app.use('/login', loginRouter);
 app.use('/favourites', favouriteRouter)
 app.use('/groups', groupsRouter)
-
-app.get('/viewGroup', function (req, res) {
-    userLoggedIn = req.session.userStatus == "loggedIn";
-
-    let groupID = req.query.sampleSelection; // groupID instead?
-
-    if(req.session.userStatus == "loggedIn"){
-        // TODO: SQL Query to get of group information and samples belonging to each group
-        // result_samples.rows
-
-        result_samples = [{
-            country: "unknown/missing",
-            host: "undefined",
-            id: "40584",
-            isolation_source: "undefined",
-            st: "72",
-            strain: "undefined"
-        }, {
-            country: "United States of America (USA)",
-            host: "Homo sapiens",
-            id: "43765",
-            isolation_source: "Nares",
-            st: "8",
-            strain: "H51158"
-        }, {
-            country: "United States of America (USA)",
-            host: "Homo sapiens",
-            id: "44028",
-            isolation_source: "Nares",
-            st: "8",
-            strain: "M42212"
-        }, {
-            country: "United States of America (USA)",
-            host: "Human",
-            id: "41697",
-            isolation_source: "Respiratory",
-            st: "8",
-            strain: "2588STDY5627603"
-        }, {
-            country: "United States of America (USA)",
-            host: "Human",
-            id: 41823,
-            isolation_source: "Respiratory",
-            st: 15,
-            strain: "undefined",
-        }, {
-            country: "unknown/missing",
-            host: "undefined",
-            id: "43502",
-            isolation_source: "Sputum",
-            st: "8",
-            strain: "COAS6087"
-        }, {
-            country: "unknown/missing",
-            host: "undefined",
-            id: "43502",
-            isolation_source: "Sputum",
-            st: "8",
-            strain: "COAS6087"
-        }];
-
-        groupInfo = {
-            group_id: "4000",
-            description: "Really close genomes to sample 44140",
-            name: "Group 4",
-            count: "4",
-            created: "2019-09-15T13:45:30",
-            modified: "2020-01-02T13:45:30"
-        };
-
-        res.render('pages/viewGroup', {
-            userLoggedIn: userLoggedIn,
-            samples: result_samples,
-            groupInfo: groupInfo
-        });
-    }
-    else{
-        res.status(404);
-        res.send({ error: 'Not found' });
-        res.render('pages/error');
-    }
-})
+app.use('/viewGroup', viewGroupRouter);
 
 
 
