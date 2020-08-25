6f7ca246345b355bf46ec5a28f6ecc73840a1135
Chaashya <fernandochaashya@gmail.com>
2020-08-25 14:41:07 +1000

    Updated create account with Knex

--- Nodewebsite/routes/createAccount.js
+++ Nodewebsite/routes/createAccount.js
@@ -1,57 +1,76 @@
-var express = require('express')
-var router = express.Router()
-let bcrypt = require('bcrypt')
+var express = require("express");
+var router = express.Router();
+let bcrypt = require("bcrypt");
 
-router.get('/', function (req, res) {
-    let userLoggedIn = false
-    if (req.session.userStatus === "loggedIn") {
-        userLoggedIn = true;
-    }
+router.get("/", function (req, res) {
+  let userLoggedIn = false;
+  if (req.session.userStatus === "loggedIn") {
+    userLoggedIn = true;
+  }
 
-    res.render('pages/createAccount', { userLoggedIn: userLoggedIn, userAlreadyExists: false });
+  res.render("pages/createAccount", {
+    userLoggedIn: userLoggedIn,
+    userAlreadyExists: false,
+  });
 });
 
+router.post("/", function (req, res) {
+  let userLoggedIn = false;
+  if (req.session.userStatus === "loggedIn") {
+    userLoggedIn = true;
+  }
 
-router.post('/', function (req, res) {
-    let userLoggedIn = false;
-    if (req.session.userStatus === "loggedIn") {
-        userLoggedIn = true;
-    }
+  var email = req.body.email;
+  var organisation = req.body.organisation;
+  var occupation = req.body.occupation;
 
-    var email = req.body.email;
-    var organisation = req.body.organisation;
-    var occupation = req.body.occupation;
+  if (email !== "") {
+    bcrypt.hash(req.body.password, 10, function (err, hashedPassword) {
+      req.knex
+        .select("*")
+        .from("registered_users")
+        .where({ email: email })
+        .then((err, result_registered_users) => {
+          //console.log(err, result_registered_users);
 
-    if (email !== "") {
-
-        bcrypt.hash(req.body.password, 10, function (err, hashedPassword) {
-
-            req.db.query('SELECT * FROM registered_users WHERE email=\'' + req.body.email + '\'', (err, result_registered_users) => {
-                console.log(err, result_registered_users);
-
-                if (result_registered_users.rows.length != 0) {
-                    console.log('User already entered into database');
-                    res.render('pages/createAccount', { userLoggedIn: userLoggedIn, userAlreadyExists: true });
-                    return;
-                } else {
-                    req.db.query('INSERT INTO registered_users (email, password, organisation, occupation) VALUES (\''
-                        + email + '\',\'' + hashedPassword + '\',\'' + organisation + '\',\'' + occupation + '\')',
-                        function (error, results, fields) {
-                            if (error) {
-                                throw error;
-                                res.redirect('/');
-                            } else {
-                                creationSuccess = true;
-                                res.redirect('/login');
-                            }
-                        });
-                }
+          //return knex('registered_users').where
+          if (result_registered_users.rows.length != 0) {
+            console.log("User already entered into database");
+            res.render("pages/createAccount", {
+              userLoggedIn: userLoggedIn,
+              userAlreadyExists: true,
             });
+            return;
+          } else {
+            req
+              .knex("registered_users")
+              .insert({
+                email: email,
+                password: hashedPassword,
+                organisation: organisation,
+                occupation: occupation,
+              })
+              .then(
+                () =>
+                  function (error, results, fields) {
+                    if (error) {
+                      throw error;
+                      res.redirect("/");
+                    } else {
+                      creationSuccess = true;
+                      res.redirect("/login");
+                    }
+                  }
+              );
+          }
         });
-    } else {
-        res.render('pages/createAccount', { userLoggedIn: userLoggedIn, userAlreadyExists: false });
-
-    }
+    });
+  } else {
+    res.render("pages/createAccount", {
+      userLoggedIn: userLoggedIn,
+      userAlreadyExists: false,
+    });
+  }
 });
 
 module.exports = router;
