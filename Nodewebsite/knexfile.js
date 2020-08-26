77aff6741aebe1ab4df08325273bea93948a8209
Chaashya <fernandochaashya@gmail.com>
2020-08-26 17:51:30 +1000

    knexfile

--- Nodewebsite/knexfile.js
+++ Nodewebsite/knexfile.js
@@ -1,24 +1,23 @@
 // Sam's Connection
-module.exports = {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    port: '5433',
-    database: 'postgres',
-    user:     'postgres',
-    password: 'postgreSAM'
-  }
-}
-
-
-// Maisie's Connection
 // module.exports = {
-//   client: 'pg',
+//   client: "pg",
 //   connection: {
-//     host: '127.0.0.1',
-//     port: '5432',
-//     database: 'staph',
-//     user:     'postgres',
-//     password: '12345'
-//   }
-// }
+//     host: "127.0.0.1",
+//     port: "5433",
+//     database: "postgres",
+//     user: "postgres",
+//     password: "postgreSAM",
+//   },
+// };
+
+//Maisie's/ Chaashya's Connection
+module.exports = {
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: "5432",
+    database: "staph",
+    user: "postgres",
+    password: "12345",
+  },
+};
