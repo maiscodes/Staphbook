f49d335aa8c4f2b7b57be4403497c44678e1f89e
maiscodes <38636861+maiscodes@users.noreply.github.com>
2020-09-03 14:57:26 +1000

    Revert "Added Maisie connection"
    This reverts commit fa8f0135b715a64f32a0ab0729e5cbeebc0e0357.

--- Nodewebsite/knexfile.js
+++ Nodewebsite/knexfile.js
@@ -1,4 +1,3 @@
-/* Sam's Connection
 module.exports = {
   client: 'pg',
   connection: {
@@ -7,18 +6,5 @@ module.exports = {
     database: 'postgres',
     user:     'postgres',
     password: 'postgreSAM'
-  }
-}
-*/
-
-// Maisie's Connection
-module.exports = {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    port: '5432',
-    database: 'staph',
-    user:     'postgres',
-    password: '12345'
   }
 }
