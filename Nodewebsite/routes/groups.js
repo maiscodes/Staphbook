1b3785fb6387ed1782b4d6f0f9e3f411ba439ad4
maiscodes <38636861+maiscodes@users.noreply.github.com>
2020-09-03 16:54:22 +1000

    Added sample count to groups endpoint

--- Nodewebsite/routes/groups.js
+++ Nodewebsite/routes/groups.js
@@ -12,13 +12,28 @@ router.get('/', function (req, res) {
             .select('*')
             .from('groups')
             .where({email: req.session.userEmail})
+            .orderBy('group_id', 'asc')
             .then((groups) => {
-                res.render('pages/groups', {
-                    userLoggedIn: userLoggedIn,
-                    groups: groups,
-                    haveGroups: true
+              let ids = [];
+              for (i = 0; i < groups.length; i ++) {
+                ids.push(groups[i].group_id);
+              }
+              req.knex
+                .select('*')
+                .from('group_samples')
+                .whereIn('group_id', ids)
+                .orderBy('group_id', 'asc')
+                .then((group_samples) => {
+                  for (i = 0; i < group_samples.length; i++) {
+                    groups[i].count = group_samples[i].sample_id.length;
+                  }
+                  res.render('pages/groups', {
+                      userLoggedIn: userLoggedIn,
+                      groups: groups,
+                      haveGroups: true
                 })
-        })
+              })
+          })
     }
     else{
         res.status(404);
