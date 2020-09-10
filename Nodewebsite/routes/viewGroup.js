4a3ccf321e7fe038927a7518e28a7b8833c3066c
Chaashya <fernandochaashya@gmail.com>
2020-09-10 12:12:03 +1000

    Revert "Merge branch 'chaas-build' of https://github.com/maiscodes/Staphbook into chaas-build"
    This reverts commit 256a6aa5b4e19198692f87b143505b18386fd509, reversing
    changes made to 05d326e7f0701174c285a5558ff71ee755bc6f0b.

--- Nodewebsite/routes/viewGroup.js
+++ Nodewebsite/routes/viewGroup.js
@@ -9,59 +9,57 @@ router.get('/viewGroup', function (req, res) {
     errorPageConfig = { description: 'group', query: 'groupId', id: groupID, endpoint: '/viewGroup', userLoggedIn: userLoggedIn };
 
     // Now execute SQL queries
-    if (userLoggedIn) {
-        try {
-            req.knex.select('*').from('groups').where({ group_id: groupID || 0 }).then((groupInfo) => {
-                groupInfo = groupInfo[0];
+    if ( userLoggedIn ) {
+      try {
+        req.knex.select('*').from('groups').where({group_id: groupID || 0}).then((groupInfo) => {
+            groupInfo = groupInfo[0];
 
-                req.knex.select('*').from('group_samples').where({ group_id: groupID || 0 }).then((row) => {
-                    row = row[0];
-                    groupSampleIds = row.sample_id || 0;
-                    //console.log(sampleIds); TODO: add handling when group doesn't have any samples
-                    req.knex.select({
-                        st: 'mlst_mlst.st', sample_id: 'sample_metadata.sample_id', metadata: 'sample_metadata.metadata',
-                        name: 'sample_sample.name', id: 'sample_sample.id'
-                    }) //TODO: refactor so id is removed
-                        .from('mlst_mlst')
-                        .innerJoin('sample_sample', 'mlst_mlst.sample_id', 'sample_sample.id')
-                        .innerJoin('sample_metadata', 'mlst_mlst.sample_id', 'sample_metadata.sample_id')
-                        .where('mlst_mlst.sample_id', 'in', groupSampleIds || 0)
-                        .then((sampleInfos) => {
-                            console.log(sampleInfos);
-                            for (sampleInfo of sampleInfos) {
-                                sampleInfo.country = sampleInfo.metadata.country;
-                                sampleInfo.strain = sampleInfo.metadata.strain;
-                                sampleInfo.host = sampleInfo.metadata.host;
-                                sampleInfo.isolation_source = sampleInfo.metadata.isolation_source;
-                            }
+            req.knex.select('*').from('group_samples').where({group_id: groupID || 0}).then((row) => {
+                row = row[0];
+                groupSampleIds = row.sample_id || 0;
+                //console.log(sampleIds); TODO: add handling when group doesn't have any samples
+                req.knex.select({st: 'mlst_mlst.st', sample_id: 'sample_metadata.sample_id', metadata: 'sample_metadata.metadata',
+                name: 'sample_sample.name', id: 'sample_sample.id'}) //TODO: refactor so id is removed
+                    .from('mlst_mlst')
+                    .innerJoin('sample_sample', 'mlst_mlst.sample_id', 'sample_sample.id')
+                    .innerJoin('sample_metadata', 'mlst_mlst.sample_id', 'sample_metadata.sample_id')
+                    .where('mlst_mlst.sample_id', 'in', groupSampleIds || 0)
+                    .then((sampleInfos) => {
+                      console.log(sampleInfos);
+                      for(sampleInfo of sampleInfos) {
+                        sampleInfo.country = sampleInfo.metadata.country;
+                        sampleInfo.strain = sampleInfo.metadata.strain;
+                        sampleInfo.host = sampleInfo.metadata.host;
+                        sampleInfo.isolation_source = sampleInfo.metadata.isolation_source;
+                      }
 
-                            res.render('pages/viewGroup', {
-                                userLoggedIn: userLoggedIn,
-                                samples: sampleInfos,
-                                groupInfo: groupInfo
-                            });
+                      res.render('pages/viewGroup', {
+                          userLoggedIn: userLoggedIn,
+                          samples: sampleInfos,
+                          groupInfo: groupInfo
+                      });
 
-                        }).catch(function (err) {
-                            console.log(err);
-                            res.render('pages/error', errorPageConfig);
-                        });
-
-                }).catch(function (err) {
-                    console.log(err);
-                    res.render('pages/error', errorPageConfig);
+                }).catch(function(err) {
+                  console.log(err);
+                  res.render('pages/error', errorPageConfig);
                 });
 
-            }).catch(function (err) {
-                console.log(err);
-                res.render('pages/error', errorPageConfig);
+            }).catch(function(err) {
+              console.log(err);
+              res.render('pages/error', errorPageConfig);
             });
 
-        } catch (err) {
+        }).catch(function(err) {
+          console.log(err);
+          res.render('pages/error', errorPageConfig);
+        });
+
+      } catch (err) {
             res.render('pages/error', errorPageConfig);
-        }
+      }
 
     }
-    else {
+    else{
         res.status(404);
         res.send({ error: 'Not found' });
         res.render('pages/error');
@@ -78,34 +76,34 @@ router.post('/removeGroupSample', function (req, res) {
     //console.log(sampleIds);
 
     if (userLoggedIn) {
-        req.knex.select('sample_id').from('group_samples').where({ group_id: groupId || 0 }).then((currentSamples) => {
-            currentSamples = currentSamples[0].sample_id;
+      req.knex.select('sample_id').from('group_samples').where({group_id: groupId || 0}).then((currentSamples) => {
+          currentSamples = currentSamples[0].sample_id;
 
-            let remainingSamples = currentSamples.filter(function (item) {
-                return !sampleIds.includes(JSON.stringify(item));
+          let remainingSamples = currentSamples.filter(function(item) {
+            return !sampleIds.includes(JSON.stringify(item));
+          })
+
+          req.knex('group_samples')
+            .where({ group_id: groupId })
+            .update({ sample_id: remainingSamples }, ['group_id', 'sample_id'])
+            .then(() => {
+              res.status(200).json({"message": "successfully removed from group"})
+              return;
             })
-
-            req.knex('group_samples')
-                .where({ group_id: groupId })
-                .update({ sample_id: remainingSamples }, ['group_id', 'sample_id'])
-                .then(() => {
-                    res.status(200).json({ "message": "successfully removed from group" })
-                    return;
-                })
-                .catch((err) => {
-                    console.log(err);
-                    res.status(401).json({ "message": "error deleting from group" })
-                    return;
-                });
-        })
             .catch((err) => {
-                console.log(err);
-                res.status(401).json({ "message": "error deleting from group" })
-                return;
+              console.log(err);
+              res.status(401).json({"message": "error deleting from group"})
+              return;
             });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(401).json({"message": "error deleting from group"})
+          return;
+        });
         return;
     }
-    res.status(401).json({ "message": "permissions error - user not logged in" })
+    res.status(401).json({"message": "permissions error - user not logged in"})
 
     // Include update time stamp here instead?
 })
