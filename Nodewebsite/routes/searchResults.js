c87f3c706d9fe983ac72e6437f154c1e09ad4be9
Sam Holden <samuel99holden@gmail.com>
2020-09-10 11:53:31 +1000

    Revert "Revert "Merge branch 'Sam-Build' into chaas-build""
    This reverts commit 8a93f845dd588d3a941555bcf0f169269b8fce20.

--- Nodewebsite/routes/searchResults.js
+++ Nodewebsite/routes/searchResults.js
@@ -1,86 +1,61 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
     let userLoggedIn = false;
+    let option = req.query.searchOption;
+
     if (req.session.userStatus === "loggedIn") {
         userLoggedIn = true;
     }
 
-    let input = req.query.searchInput;
-    let option = req.query.searchOption;
-    let number = 0;
+    res.locals.input = req.query.searchInput;
+    res.locals.option = option;
+    res.locals.number = 0;
+    res.locals.userLoggedIn = userLoggedIn;
+
     if (req.query.searchInput) {
         if(option === "Sequence"){
-            req.db.query("SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-                "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-                "sample_metadata.metadata->>'isolation_source' AS isolation_source " +
-                "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
-                "WHERE mlst_mlst.sample_id IN (SELECT sample_id FROM sample_metadata " +
-                "WHERE st = '" + input + "')", (err, result_samples) => {
-                console.log(err, result_samples);
-                number = result_samples.rows.length;
-                res.render('pages/searchResults', { samples: result_samples.rows, input: input, option: option, number: number, userLoggedIn: userLoggedIn });
-            });
-        } else if(option === "Location"){
-            req.db.query("SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-                "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-                "sample_metadata.metadata->>'isolation_source' AS isolation_source " +
-                "FROM sample_metadata INNER JOIN mlst_mlst ON sample_metadata.sample_id=mlst_mlst.sample_id " +
-                "WHERE sample_metadata.sample_id IN (SELECT sample_id FROM sample_metadata " +
-                "WHERE metadata->>'country' ILIKE '%" + input + "%')", (err, result_samples) => {
-                console.log(err, result_samples);
-                number = result_samples.rows.length;
-                res.render('pages/searchResults', { samples: result_samples.rows, input: input, option: option, number: number, userLoggedIn: userLoggedIn });
-            });
-        } else if(option === "Strain Name"){
-            req.db.query("SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-                "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-                "sample_metadata.metadata->>'isolation_source' AS isolation_source " +
-                "FROM sample_metadata INNER JOIN mlst_mlst ON sample_metadata.sample_id=mlst_mlst.sample_id " +
-                "WHERE sample_metadata.sample_id IN (SELECT sample_id FROM sample_metadata " +
-                "WHERE metadata->>'strain' ILIKE '%" + input + "%')", (err, result_samples) => {
-                console.log(err, result_samples);
-                number = result_samples.rows.length;
-                res.render('pages/searchResults', { samples: result_samples.rows, input: input, option: option, number: number, userLoggedIn: userLoggedIn });
-            });
-        } else if(option === "Host"){
-            req.db.query("SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-                "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-                "sample_metadata.metadata->>'isolation_source' AS isolation_source " +
-                "FROM sample_metadata INNER JOIN mlst_mlst ON sample_metadata.sample_id=mlst_mlst.sample_id " +
-                "WHERE sample_metadata.sample_id IN (SELECT sample_id FROM sample_metadata " +
-                "WHERE metadata->>'host' ILIKE '%" + input + "%')", (err, result_samples) => {
-                console.log(err, result_samples);
-                number = result_samples.rows.length;
-                res.render('pages/searchResults', { samples: result_samples.rows, input: input, option: option, number: number, userLoggedIn: userLoggedIn });
-            });
-        } else if(option === "Source"){
-            req.db.query("SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-                "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-                "sample_metadata.metadata->>'isolation_source' AS isolation_source " +
-                "FROM sample_metadata INNER JOIN mlst_mlst ON sample_metadata.sample_id=mlst_mlst.sample_id " +
-                "WHERE sample_metadata.sample_id IN (SELECT sample_id FROM sample_metadata " +
-                "WHERE metadata->>'isolation_source' ILIKE '%" + input + "%')", (err, result_samples) => {
-                console.log(err, result_samples);
-                number = result_samples.rows.length;
-                res.render('pages/searchResults', { samples: result_samples.rows, input: input, option: option, number: number, userLoggedIn: userLoggedIn });
-            });
-        } else if(option === "Sample"){
-            req.db.query("SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-                "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-                "sample_metadata.metadata->>'isolation_source' AS isolation_source " +
-                "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
-                "WHERE mlst_mlst.sample_id IN (SELECT sample_id FROM sample_metadata " +
-                "WHERE sample_id = '" + input + "')", (err, result_samples) => {
-                console.log(err, result_samples);
-                number = result_samples.rows.length;
-                res.render('pages/searchResults', { samples: result_samples.rows, input: input, option: option, number: number, userLoggedIn: userLoggedIn });
-            });
+            res.locals.getSamples = req.knex.select('sample_id').from('sample_metadata').where({st: input});
         }
+        else if(option === "Location"){
+            res.locals.getSamples = req.knex.select('sample_id').from('sample_metadata').whereRaw('metadata->>? ILIKE ?', ['country', '%'+input+'%']);
+        }
+        else if(option === "Strain Name"){
+            res.locals.getSamples = req.knex.select('sample_id').from('sample_metadata').whereRaw('metadata->>? ILIKE ?', ['strain', '%'+input+'%']);
+        }
+        else if(option === "Host"){
+            res.locals.getSamples = req.knex.select('sample_id').from('sample_metadata').whereRaw('metadata->>? ILIKE ?', ['host', '%'+input+'%']);
+        }
+        else if(option === "Source"){
+            res.locals.getSamples = req.knex.select('sample_id').from('sample_metadata').whereRaw('metadata->>? ILIKE ?', ['isolation_source', '%'+input+'%']);
+        }
+        else if(option === "Sample"){
+            res.locals.getSamples = req.knex.select('sample_id').from('sample_metadata').where({sample_id: input})
+        }
+
+        next()
     } else {
         res.redirect('/');
     }
 });
 
+router.get('/', function(req, res){
+    req.knex.select({st: 'mlst_mlst.st', sample_id: 'sample_metadata.sample_id', metadata: 'sample_metadata.metadata',
+        name: 'sample_sample.name', id: 'sample_sample.id'})
+        .from('mlst_mlst')
+        .innerJoin('sample_sample', 'mlst_mlst.sample_id', 'sample_sample.id')
+        .innerJoin('sample_metadata', 'mlst_mlst.sample_id', 'sample_metadata.sample_id')
+        .where('mlst_mlst.sample_id', 'in', res.locals.getSamples)
+        .then((same_samples) => {
+            for (const same of same_samples) {
+                same.country = same.metadata.country;
+                same.strain = same.metadata.strain;
+                same.host = same.metadata.host;
+                same.isolation_source = same.metadata.isolation_source;
+            }
+            let number = res.locals.number + same_samples.length;
+            res.render('pages/searchResults', { samples: same_samples, input: res.locals.input, option: res.locals.option, number: number, userLoggedIn: res.locals.userLoggedIn });
+        });
+})
 module.exports = router;
