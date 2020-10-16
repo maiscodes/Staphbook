fb7d7bfc07896753486bb2323df903e211df6720
Sam Holden <samuel99holden@gmail.com>
2020-10-16 10:56:49 +1000

    Remove old commented section

--- Nodewebsite/routes/favourites.js
+++ Nodewebsite/routes/favourites.js
@@ -40,40 +40,6 @@ router.get('/', function (req, res) {
                         res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: sampleInfos, haveFavs: true });
                     });
             });
-
-
-
-        // req.db.query("SELECT * FROM user_favorites WHERE email='" + email + "'", (err, fav_results) => {
-        //     console.log(err, fav_results);
-        //     if (fav_results.rows.length > 0) {
-        //         haveFavs = true;
-        //         haveSugs = true;
-        //         let selectSQL = "";
-        //         selectSQL = "SELECT mlst_mlst.st, sample_metadata.sample_id, metadata->>'country' AS country, " +
-        //             "sample_metadata.metadata->>'strain' AS strain, sample_metadata.metadata->>'host' AS host, " +
-        //             "sample_metadata.metadata->>'isolation_source' AS isolation_source, sample_metadata.metadata->>'date_collected' AS date " +
-        //             "FROM mlst_mlst  INNER JOIN sample_metadata ON mlst_mlst.sample_id=sample_metadata.sample_id " +
-        //             "WHERE mlst_mlst.sample_id IN (SELECT sample_id FROM sample_metadata " +
-        //             "WHERE";
-        //
-        //         for (let i = 0; i < fav_results.rows.length; i++) {
-        //             if (i === 0) {
-        //                 selectSQL += " sample_id = " + fav_results.rows[i].sample_id + "";
-        //             }
-        //             selectSQL += " OR sample_id = " + fav_results.rows[i].sample_id + "";
-        //         }
-        //
-        //         req.db.query(selectSQL + ");", (err, favorites) => {
-        //             console.log(err, favorites);
-        //             console.log(haveFavs);
-        //             res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: favorites.rows, haveFavs: haveFavs });
-        //         });
-        //         // no favourites found for current user
-        //     } else {
-        //         res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: 0, haveFavs: false });
-        //     }
-        // });
-
     }
 });
 
