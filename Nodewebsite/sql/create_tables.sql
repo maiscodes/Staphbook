f2e4d606ce2c0f2c67ae1fddf1d42cb4107fc9bc
Sam Holden <samuel99holden@gmail.com>
2020-10-19 15:42:56 +1000

    Update create_tables.sql
    Include insert public all users

--- Nodewebsite/sql/create_tables.sql
+++ Nodewebsite/sql/create_tables.sql
@@ -29,6 +29,9 @@ TABLESPACE pg_default;
 ALTER TABLE public.registered_users
     OWNER to staphbook;
 
+INSERT INTO public.registered_users (email)
+    VALUES ('_public_all_users_');
+
 CREATE TABLE public.groups
 (
     group_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
