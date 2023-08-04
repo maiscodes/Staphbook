7c2ac0cde88cd7e8bc79060ef88f20aaf2c15094
Charlie Turner <84701489+charlie-turner-314@users.noreply.github.com>
2023-08-04 16:01:33 +1000

    fix favs page

--- Nodewebsite/routes/favourites.js
+++ Nodewebsite/routes/favourites.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 const getGatherData = require('../utils/getGatherData')
 
-router.get('/', async function (req, res) {
+router.get('/', async function(req, res) {
     // Initial values
     let userLoggedIn = false;
     let favorites = [];
@@ -26,66 +26,69 @@ router.get('/', async function (req, res) {
                 }
             res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: sampleInfos, haveFavs: true });
             */
-        
+
         let favouriteIds = [];
 
         let favs = await req.knex
             .select('*')
             .from('user_favorites')
-            .where({email: email});
+            .where({ email: email });
 
         favs.forEach(fav => {
             favouriteIds.push(fav.sample_id);
-            });
+        });
         favorites = await Promise.all(favouriteIds.map(async (f) =>
             // get the metadata
-        getGatherData(f) 
+            getGatherData(f)
         ));
         console.log(favorites);
         res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: favorites, haveFavs: true });
 
-            /*
-            .then(favs => {
-                req.knex.select({st: 'mlst_mlst.st', sample_id: 'sample_metadata.sample_id', metadata: 'sample_metadata.metadata',
-                    name: 'sample_sample.name', id: 'sample_sample.id'}) //TODO: refactor so id is removed
-                    .from('mlst_mlst')
-                    .innerJoin('sample_sample', 'mlst_mlst.sample_id', 'sample_sample.id')
-                    .innerJoin('sample_metadata', 'mlst_mlst.sample_id', 'sample_metadata.sample_id')
-                    .modify(function (queryBuilder) {
-                        queryBuilder.where('mlst_mlst.sample_id', favs[0].sample_id || 0);
-                        for (i = 1; i < favs.length; i++) {
-                            queryBuilder.orWhere('mlst_mlst.sample_id', favs[i].sample_id);
-                        }
-                    })
-                    .then((sampleInfos) => {
-                        //console.log(sampleInfos);
-                        for (sampleInfo of sampleInfos) {
-                            sampleInfo.country = sampleInfo.metadata.country;
-                            sampleInfo.strain = sampleInfo.metadata.strain;
-                            sampleInfo.host = sampleInfo.metadata.host;
-                            sampleInfo.isolation_source = sampleInfo.metadata.isolation_source;
-                        }
-                        
-                        res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: sampleInfos, haveFavs: true });
-                    });
-            });*/
+        /*
+        .then(favs => {
+            req.knex.select({st: 'mlst_mlst.st', sample_id: 'sample_metadata.sample_id', metadata: 'sample_metadata.metadata',
+                name: 'sample_sample.name', id: 'sample_sample.id'}) //TODO: refactor so id is removed
+                .from('mlst_mlst')
+                .innerJoin('sample_sample', 'mlst_mlst.sample_id', 'sample_sample.id')
+                .innerJoin('sample_metadata', 'mlst_mlst.sample_id', 'sample_metadata.sample_id')
+                .modify(function (queryBuilder) {
+                    queryBuilder.where('mlst_mlst.sample_id', favs[0].sample_id || 0);
+                    for (i = 1; i < favs.length; i++) {
+                        queryBuilder.orWhere('mlst_mlst.sample_id', favs[i].sample_id);
+                    }
+                })
+                .then((sampleInfos) => {
+                    //console.log(sampleInfos);
+                    for (sampleInfo of sampleInfos) {
+                        sampleInfo.country = sampleInfo.metadata.country;
+                        sampleInfo.strain = sampleInfo.metadata.strain;
+                        sampleInfo.host = sampleInfo.metadata.host;
+                        sampleInfo.isolation_source = sampleInfo.metadata.isolation_source;
+                    }
+                    
+                    res.render('pages/favourites', { userLoggedIn: userLoggedIn, favorites: sampleInfos, haveFavs: true });
+                });
+        });*/
+    }
+    else {
+        // redirect to '/'
+        res.redirect('/');
     }
 });
 
-router.post('/', function (req, res) {
+router.post('/', function(req, res) {
     let userLoggedIn = req.session.userStatus === "loggedIn";
 
     let sampleID = req.body.favouritedSampleID;
 
-    if(userLoggedIn) {
+    if (userLoggedIn) {
         let value = req.session.userEmail;
         let email = decodeURIComponent(value);
 
         req.knex('user_favorites')
-            .where({email: email, sample_id: sampleID})
+            .where({ email: email, sample_id: sampleID })
             .del()
             .then((deleted) => {
-                console.log("Deleted: " + deleted + ' row, with sample_id='+sampleID);
                 res.redirect('/favourites');
             })
     }
